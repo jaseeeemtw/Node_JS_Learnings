eefd93ee5395b4fe742cb34892add8da2bcb9a54
jaseeeemtw <120625185+jaseeeemtw@users.noreply.github.com>
2023-05-28 09:52:19 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://jjmongo:roQ0hp7FUj1EUM1L@cluster0.7d13b.mongodb.net/shop?retryWrites=true&w=majority"
+    "add_connection_string"
   )
     .then((client) => {
       console.log("Connected!");
